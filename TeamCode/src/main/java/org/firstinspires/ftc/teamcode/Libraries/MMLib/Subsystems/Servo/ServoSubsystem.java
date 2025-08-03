8e8599592943bb6130c0bc20c388eaa27c63ba84
ori coval <oricoval@gmail.com>
2025-08-03 15:20:25 +0300

    settign servo direction and offset during reset

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Libraries/MMLib/Subsystems/Servo/ServoSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Libraries/MMLib/Subsystems/Servo/ServoSubsystem.java
@@ -189,13 +189,19 @@ public class ServoSubsystem extends MMSubsystem {
         ArrayList<CuttleServo> tempList = new ArrayList<>();
         for(CuttleServo servo : servoList){
             if(servo.getFtcServo()){
-                tempList.add(new CuttleServo(MMRobot.getInstance().currentOpMode.hardwareMap, servo.getServoName()));
+                tempList.add(new CuttleServo(MMRobot.getInstance().currentOpMode.hardwareMap, servo.getServoName())
+                        .setDirection(servo.getDirection())
+                        .setOffset(servo.getOffset()));
 
             } else if(servo.hub.getHubName().equals(MMRobot.getInstance().controlHub.getHubName())){
-                tempList.add(new CuttleServo(MMRobot.getInstance().controlHub, servo.port));
+                tempList.add(new CuttleServo(MMRobot.getInstance().controlHub, servo.port)
+                        .setDirection(servo.getDirection())
+                        .setOffset(servo.getOffset()));
             }
             else {
-                tempList.add(new CuttleServo(MMRobot.getInstance().expansionHub, servo.port));
+                tempList.add(new CuttleServo(MMRobot.getInstance().expansionHub, servo.port)
+                        .setDirection(servo.getDirection())
+                        .setOffset(servo.getOffset()));
             }
         }
 
